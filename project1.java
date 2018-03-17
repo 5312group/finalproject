3e537352daab9a67689c7ec309aeb6c324a1685f
shiqiqi1077 <33248792+shiqiqi1077@users.noreply.github.com>
2018-03-18 00:08:53 +0800

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class project1 extends JFrame{
+	private JLabel messageLabel;
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenu fontMenu;
+	private JMenuItem newItem;
+	private JMenuItem openItem;
+	private JMenuItem saveItem;
+	private JMenuItem saveasItem;
+	public project1(){
+		setTitle("Text Editor");
+		setSize(400,300)
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new project1();
+	}
+}
