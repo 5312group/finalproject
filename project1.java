bf8c8032e0da6d07fbab09a3f4a99dbc4c73a8d9
shiqiqi <shiqiqi@github.com>
2018-03-19 19:12:56 +0800

    1
    main frame and some functions achieved

--- project1.java
+++ project1.java
@@ -1,69 +1,80 @@
+package texteditor;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.datatransfer.*;
+import java.io.*;
 
-public class project2 extends JFrame{
-	private JLabel messageLabel;
+public class project1 extends JFrame implements ActionListener{
+	
+	private static final long serialVersionUID = 1L;
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
 	private JMenu fontMenu;
-	private JMenuItem newItem;
-	private JMenuItem openItem;
-	private JMenuItem saveItem;
-	private JMenuItem saveasItem;
-	private JMenuItem exitItem;
+	private JMenu editMenu;
 	private JList menuList;
 	private String[] fontChoice = {"Monospaced", "Serif","SansSerif"};
 	private JRadioButtonMenuItem itaButton;
 	private JRadioButtonMenuItem bolButton;
-	private JTextArea theArea;
-	public project2(){
-		setTitle("Text Editor");
-		pack();
-		setVisible(true);
-		//JScrollPane scroll =new JScrollPane(theArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		//add(scroll,BorderLayout.CENTER);
-		//theArea = new JTextArea();
-		//theArea.setEditable(false);
-		//getContentPane().add(new JScrollPane(theArea));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		buildMenuBar();
+	JTextArea theArea = new JTextArea();
+
+	File fnameContainer;
+	
+	public project1(){
 		
-	}
-	private void buildMenuBar(){
-		//create the menu bar
-		menuBar = new JMenuBar();
-		//create two menus
-		buildFileMenu();
-		buildFontMenu();
-		//add files to main menubar
-		menuBar.add(fileMenu);
-		menuBar.add(fontMenu);
+		
+		Container con= getContentPane();
+		JMenuBar menuBar = new JMenuBar();
+		JPanel panel1 = new JPanel();
+		
+		JMenu file = new JMenu("File");
+		JMenu font = new JMenu("Font");
+		JMenu edit = new JMenu("Edit");
+		
+		con.setLayout(new BorderLayout());
+		JScrollPane jspText = new JScrollPane(theArea);
+		jspText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		jspText.setVisible(true);
+		Font fnt=new Font("Arial",Font.PLAIN,15);
+		theArea.setFont(fnt);
+		theArea.setLineWrap(true);
+		theArea.setWrapStyleWord(true);
+		
+		con.add(jspText);
+		
+		createMenuItem(file,"New");
+		createMenuItem(file,"Open");
+		createMenuItem(file,"Save");
+		createMenuItem(file,"Save As");
+		createMenuItem(file,"Exit");
+		createMenuItem(edit,"Cut");
+		createMenuItem(edit,"Copy");
+		createMenuItem(edit,"Paste");
+		
+		menuBar.add(file);
+		menuBar.add(buildFontMenu());
+		menuBar.add(edit);
+	
+		
 		setJMenuBar(menuBar);
-	
+		
+		
+		setSize(300,400);
+		setTitle("Text Editor for project IS5312");
+		setVisible(true);
+		
+		
 		
 	}
-	
-	private void buildFileMenu(){
-		fileMenu = new JMenu("File");
-		newItem = new JMenuItem("new");
-		openItem = new JMenuItem("open");
-		saveItem = new JMenuItem("save");
-		saveasItem = new JMenuItem("save as");
-		exitItem = new JMenuItem("exit");
-	
-		
-		fileMenu.add(newItem);
-		fileMenu.add(openItem);
-		fileMenu.add(saveItem);
-		fileMenu.add(saveasItem);
-		fileMenu.add(exitItem);
-		//fileMenu.getAccessibleContext().setAccessibleDescription("the first menu in this program");
+	public void createMenuItem(JMenu jm,String txt){
+		JMenuItem jmi=new JMenuItem(txt);
+		jmi.addActionListener(this);
+		jm.add(jmi);
 	}
 	
-	private void buildFontMenu(){
+	public JMenu buildFontMenu(){
 			
-			//fileMenu.add(newItem)
+			
 			
 			itaButton = new JRadioButtonMenuItem("Italic",true);
 			bolButton = new JRadioButtonMenuItem("Bold",true);
@@ -77,11 +88,102 @@ public class project2 extends JFrame{
 			fontMenu.addSeparator();
 			fontMenu.add(itaButton);
 			fontMenu.add(bolButton);
-			
+			return fontMenu;
 		}
 		
+		
+
+public void actionPerformed(ActionEvent e){
+		JFileChooser jfc = new JFileChooser();
+		if (e.getActionCommand().equals("New"))
+		{
+			this.setTitle("Untitled.txt");
+			theArea.setText(null);
+			fnameContainer = null;
+	}else if(e.getActionCommand().equals("Open")){
+				//open
+				int ret=jfc.showDialog(null,"Open");
+				
+				if(ret == JFileChooser.APPROVE_OPTION)
+				{
+					try{
+						File fyl=jfc.getSelectedFile();
+						OpenFile(fyl.getAbsolutePath());
+						this.setTitle(fyl.getName()+ " - Notepad");
+						fnameContainer=fyl;
+					}catch(IOException ers){}
+				} 
+				 
+			}else if(e.getActionCommand().equals("save")) {
+				if (fnameContainer !=null) {
+					jfc.setCurrentDirectory(fnameContainer);
+					jfc.setSelectedFile(fnameContainer);
+					
+				}else {
+					
+					jfc.setSelectedFile(new File("untitled.txt"));
+				}
+				int ret=jfc.showSaveDialog(null);
+				if(ret==JFileChooser.APPROVE_OPTION) {
+					
+					try {
+						File fyl = jfc.getSelectedFile();
+						SaveFile(fyl.getAbsolutePath());
+						this.setTitle(fyl.getName()+"-notepad");
+						fnameContainer=fyl;
+						
+					}catch (Exception ers2) {}
+				}
+			} else if(e.getActionCommand().equals("Exit")) {
+				Exiting();
+				
+			} else if(e.getActionCommand().equals("Cut")) {
+				theArea.cut();
+				
+			} else if(e.getActionCommand().equals("Copy")) {
+				theArea.copy();
+				
+			}else if(e.getActionCommand().equals("Paste")) {
+				theArea.paste();
+				
+			}
+	
+	
+	
+	
+	
+	}
+	
+	public void OpenFile(String fname) throws IOException {	
+			//open file code here
+			BufferedReader d=new BufferedReader(new InputStreamReader(new FileInputStream(fname)));
+			String l;
+			//clear the textbox
+			theArea.setText("");
+		
+			setCursor(new Cursor(Cursor.WAIT_CURSOR));
+				
+			while((l=d.readLine())!= null) {
+				theArea.setText(theArea.getText()+l+"\r\n");
+			}
+			
+			setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			d.close();
+		}
+	
+	public void SaveFile(String fname) throws IOException {
+		setCursor(new Cursor(Cursor.WAIT_CURSOR));
+		DataOutputStream o=new DataOutputStream(new FileOutputStream(fname));
+		o.writeBytes(theArea.getText());
+		o.close();		
+		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+	}
+	public void Exiting() {
+		
+		System.exit(0);
+	}
 	public static void main(String[] args) {
-		new project2();
-		//made change in 12.48 am --test
+		 new project1();
+		
 	}
 }
